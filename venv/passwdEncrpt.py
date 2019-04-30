36606164814baf0eb3cc7851261df3ac216bccf0
pmetheus <yuzhelu@gmail.com>
2019-04-30 17:42:50 -0600

    Finished Project

--- venv/passwdEncrpt.py
+++ venv/passwdEncrpt.py
@@ -1,4 +1,5 @@
-#runs on Python 3.6
+#runs on Python 3.6.5
+
 from hashlib import sha256
 from passlib.hash import pbkdf2_sha256
 import tkinter
@@ -10,42 +11,39 @@ import os
 #define valid password entries
 ALPHABET = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_')
 
-
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 read_dotenv(dotenv_path)
 
 SECRET_KEY = os.getenv('SECRET_KEY').encode('utf-8')
 
-
 # 1 takes two strings and generates a hex represenation of the hash.
-def get_hexdigest(salt, password):
-    return bytes(sha256(salt + password).hexdigest(), 'utf-8')
+def get_hexdigest(plaintext, key):
+    return bytes(sha256(plaintext + key).hexdigest(), 'utf-8')  # construtcs SHA256 Hash object and returns
+
 
 # 2 adds in secret key to generate a more secure password, less guessable.
-def make_password(plaintext, service):
-    salt = get_hexdigest(SECRET_KEY, service)[:20]
-    hsh = get_hexdigest(salt, plaintext)
-    return salt+hsh
+def make_password(password, service):
+    salt = get_hexdigest(SECRET_KEY, service)[:20]          # generates a salt based on secret key and the service being used
+    hsh = get_hexdigest(salt, password)                    # get another SHA256 hash object using the former SHA256 object and general password
+    return salt+hsh                                         # combine to one large SHA256 hash object
 
 
 def password(plaintext, service, length = 10, alphabet = ALPHABET):
-    raw_hexdigest = make_password(plaintext, service)
+    raw_hexdigest = make_password(plaintext, service)                       # Get hashed passcode using general password and service used
 
-    print(raw_hexdigest)
-    #convert hexdigest to decimal
+    # convert hexdigest to decimal
     num = int(raw_hexdigest, 16)
 
-    #what base to convert num into
+    # set the modulus based on the allowed alphabet size
     num_chars = len(alphabet)
 
     # build up the new password one char at a time, up to certain length
     chars = []
     while len(chars) < length:
-        num, idx = divmod(num, num_chars) #takes two numbers and returns a tuple of qotient and remainder
+        num, idx = divmod(num, num_chars)                                   #takes two numbers and returns a tuple of qotient and remainder
+        chars.append(ALPHABET[idx])                                         # uses the index returned to match with an index of the alphabet
 
-        chars.append(ALPHABET[idx]) # uses the index returned to match with an index of the alphabet
-
-    return ''.join(chars)
+    return ''.join(chars)                                                   # returns new password
 
 
 #hexdIG = get_hexdigest('reddit'.encode('utf-8'),'password'.encode('utf-8'))
@@ -60,13 +58,6 @@ window = Tk()
 window.title('Password Generator')
 window.geometry('400x250')
 
-'''
-SK_label = Label(text='Secret Key: ')
-SK_label.grid(column = 0, row = 0)
-skeyEntry =  Entry(window, width = 10)
-skeyEntry.grid(column = 1, row = 0)
-'''
-
 Serv_Label = Label(text='Web Service: ')
 Serv_Label.grid(column = 0, row=1)
 servEntry = Entry(window, width=10)
@@ -79,12 +70,10 @@ gpEntry.grid(column = 1, row = 2)
 
 
 def clicked():
-    SECRET_KEY = skeyEntry.get().encode('utf-8')
     service = servEntry.get().encode('utf-8')
     pswd = gpEntry.get().encode('utf-8')
     EncryptedPass = password(pswd, service)
-
-    messagebox.showinfo('Output', EncryptedPass)
+    messagebox.showinfo('New Password', EncryptedPass)
 
 
 btn = Button(window, text='Run Program', command=clicked)
